926fef4cc2ed0ae69aa3466b4432a2fdf15aa67f
aomushi0710 <aomushi0710@gmail.com>
2025-08-05 22:21:00 +0900

    フォントを変更、それに伴い一部配置を修正、action_buttonのUIを新しいものに

--- PieChart_action.gd
+++ PieChart_action.gd
@@ -9,7 +9,31 @@ extends Control
 var actions: Array[Action]
 var chances: Array[int]
 
+# 技の確率表示円グラフ生成
 func _ready() -> void:
-	for i in range(4):
-		nodes[i].value = chances[i]
-		nodes[i].tint_progress = actions[i].element[0].color
+	var color_list: Array[Color] ## これまでの色の履歴を保持
+	var sum_chance: int = 0 ## それまでの合計の確率
+	
+	for i in len(actions):
+		## 被らないようにcolor_checker関数を通してから、使用する色を取得
+		## (複数属性あるものも、とりあえず1つ目の属性のみにする)
+		var color: Color = color_checker(color_list, actions[i].element[0].color)
+		sum_chance += chances[i]
+		
+		nodes[i].tint_progress = color # 色設定
+		nodes[i].value = sum_chance # 数値設定
+		
+		color_list.append(color) # 使用済みの色を登録
+		if sum_chance >= 100: # もし既に100%を越えていたら
+			break
+		
+
+## 既に使用された色のリストcolor_listと、追加したい色colorを引数とする。
+## color_listの中に、colorと重複する色が含まれていた場合に、色をずらして返す関数
+func color_checker(color_list: Array[Color], color: Color) -> Color:
+	for c: Color in color_list:
+		if color.is_equal_approx(c): # もし色が一緒なら少し色をずらす
+			color.v -= 0.15
+			if color.v < 0: # オーバーフロー防止
+				color.v += 1
+	return color
---
+++ action_button.gd
@@ -0,0 +1,51 @@
+extends Button
+
+@onready var element: TextureRect = $element
+@onready var background: TextureRect = $background
+
+var action: Action ## 技
+var n: int = 0 ## 属性アイコン点滅用
+
+func _ready() -> void:
+	var gradient: Gradient = background.texture.gradient
+	action = Global.action_data[55]
+	if action == null:
+		text = ""
+		element.hide()
+		gradient.colors = PackedColorArray([Color.BLACK])
+	else:
+		text = action.name
+		element.texture = action.element[0].icon
+		if len(action.element) <= 1:
+			gradient.colors = PackedColorArray(
+				[action.element[0].color, action.element[0].color.darkened(0.2)])
+			gradient.offsets = PackedFloat32Array([1.0 / 3.0, 2.0 / 3.0])
+		else:
+			var color_list: PackedColorArray
+			for ele: Element in action.element: # 属性の色をリストに全て登録
+				color_list.append(ele.color)
+			gradient.colors = color_list
+			
+			var offset_list: PackedFloat32Array
+			for i in len(color_list): # グラデーションの位置をリストに全て登録
+				offset_list.append((i + 1.0) / (len(color_list) + 1.0)) # 色数によって変動
+			gradient.offsets = offset_list
+			
+			blink()
+
+## 点滅関数
+func blink() -> void:
+	var tween = create_tween().bind_node(element)
+	tween.set_loops() # 以下をループ
+	tween.tween_property(element, "self_modulate:a", 0, 1)\
+	.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUINT)
+	tween.tween_callback(Callable(self, "change"))
+	tween.tween_property(element, "self_modulate:a", 1, 1)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
+
+## blink()用更新関数
+func change() -> void:
+	n += 1
+	if n >= len(action.element): # 無効なindexを取らないように初期化
+		n = 0
+	element.texture = action.element[n].icon
